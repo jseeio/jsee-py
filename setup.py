47e0c8c488e1ff7d142731c4f3ff59c27ff62468
zemlyansky <anton.zemlyansky@gmail.com>
2021-12-23 15:14:27 +0200

    .

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name='jsee',
+  version='0.0.1',
+  scripts=['jsee'],
+  description='Simple GUI for processing tasks',
+  author='Anton Zemlyansky',
+  author_email='anton@zemlyansky.com',
+  url='https://github.com/jseeio/jsee-py',
+)
